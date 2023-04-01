dfe94781dd74bc2d627d601dc850501a932b9697
Thevenet Aymeric <aymeric.thevenet@iut-rodez.fr>
2023-04-01 16:20:09 +0200

    possibilité de consulter les humeurs

--- CYMDroid/app/src/main/java/com/example/cymdroid/FragmentHistorique.java
+++ CYMDroid/app/src/main/java/com/example/cymdroid/FragmentHistorique.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,7 +37,7 @@ import java.util.Map;
 
 public class FragmentHistorique extends Fragment implements AdapterView.OnItemClickListener {
 
-    private static final String URL_LAST_HUMEUR = "https://cymyellow1.000webhostapp.com/API/fiveLastHumeurs";
+    private static final String URL_LAST_HUMEUR = "https://cymyellow1.000webhostapp.com/API/fiveLastHumeurs/%s";
     private ListView listeHumeurs;
     private String emojis[];
 
@@ -45,6 +46,8 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
     private String dates[];
     private TextView aaa;
     private RequestQueue fileRequete;
+
+    private ArrayList<String> descriptionHumeurs;
 
     public FragmentHistorique() {
     }
@@ -78,48 +81,49 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
 //        listeHumeurs.setOnItemClickListener(this);
 
         listeHumeurs = (ListView) vueDuFragment.findViewById(R.id.listeHumeurs);
-
-        //Création de la ArrayList qui nous permettra de remplir la listView
-        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
-
-        //On déclare la HashMap qui contiendra les informations pour un item
-        HashMap<String, String> map;
-
-        //Création d'une HashMap pour insérer les informations du premier item de notre listView
-        map = new HashMap<String, String>();
-        //on insère un élément emoji que l'on récupérera dans le textView emoji créé dans le fichier row_item.xml
-        map.put("emoji", "test");
-        //on insère un élément humeur que l'on récupérera dans le textView humeur créé dans le fichier row_item.xml
-        map.put("humeur", "test");
-        //on insère un élément date que l'on récupérera dans le textView date créé dans le fichier row_item.xml
-        map.put("date", "test");
-        //on ajoute cette hashMap dans la arrayList
-        listItem.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("emoji", "test");
-        map.put("humeur", "test");
-        map.put("date", "test");
-        listItem.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("emoji", "test");
-        map.put("humeur", "test");
-        map.put("date", "test");
-        listItem.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("emoji", "test");
-        map.put("humeur", "test");
-        map.put("date", "test");
-        listItem.add(map);
-
-        //Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre list (listItem) dans la vue row_item
-        SimpleAdapter adaptateur = new SimpleAdapter (getActivity(), listItem, R.layout.row_item,
-                new String[] {"emoji", "humeur", "date"}, new int[] {R.id.emoji, R.id.humeur, R.id.date});
-
-        //On attribue à notre listView l'adapter que l'on vient de créer
-        listeHumeurs.setAdapter(adaptateur);
+//
+//        //Création de la ArrayList qui nous permettra de remplir la listView
+//        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+//
+//        //On déclare la HashMap qui contiendra les informations pour un item
+//        HashMap<String, String> map;
+//
+//        //Création d'une HashMap pour insérer les informations du premier item de notre listView
+//        map = new HashMap<String, String>();
+//        //on insère un élément emoji que l'on récupérera dans le textView emoji créé dans le fichier row_item.xml
+//        map.put("emoji", "test");
+//        //on insère un élément humeur que l'on récupérera dans le textView humeur créé dans le fichier row_item.xml
+//        map.put("humeur", "test");
+//        //on insère un élément date que l'on récupérera dans le textView date créé dans le fichier row_item.xml
+//        map.put("date", "test");
+//        //on ajoute cette hashMap dans la arrayList
+//        listItem.add(map);
+//
+//        map = new HashMap<String, String>();
+//        map.put("emoji", "test");
+//        map.put("humeur", "test");
+//        map.put("date", "test");
+//        listItem.add(map);
+//
+//        map = new HashMap<String, String>();
+//        map.put("emoji", "test");
+//        map.put("humeur", "test");
+//        map.put("date", "test");
+//        listItem.add(map);
+//
+//        map = new HashMap<String, String>();
+//        map.put("emoji", "test");
+//        map.put("humeur", "test");
+//        map.put("date", "test");
+//        listItem.add(map);
+//
+//
+//        //Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre list (listItem) dans la vue row_item
+//        SimpleAdapter adaptateur = new SimpleAdapter (getActivity(), listItem, R.layout.row_item,
+//                new String[] {"emoji", "humeur", "date"}, new int[] {R.id.emoji, R.id.humeur, R.id.date});
+//
+//        //On attribue à notre listView l'adapter que l'on vient de créer
+//        listeHumeurs.setAdapter(adaptateur);
 
         // TODO tester si la connexion est reussi
 //        vueDuFragment.setVisibility(View.INVISIBLE);
@@ -156,53 +160,25 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+//        System.out.println("a aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: ");
+//        if (descriptionHumeurs != null) {
+//            System.out.println("b : " + listeHumeurs.toString());
+//            Toast.makeText(getActivity(), descriptionHumeurs.get(i), Toast.LENGTH_LONG).show();
+//        }
     }
 
     private void getFiveHumeurs(String codeCompte, String apiKey) {
+        String url = String.format(URL_LAST_HUMEUR, codeCompte);
         System.out.println("code = " + codeCompte);
         System.out.println("API = " + apiKey);
-        boolean toutOk;
-        /*
-         * préparation du nouveau client, à ajouter, en tant qu'objet Json
-         * Les informations le concernant sont renseignées avec des valeurs par défaut,
-         * sauf le nom du magasin qui est celui renseigné par l'utilisateur
-         */
-        toutOk = true;
-        JSONArray objetAEnvoyer = new JSONArray();
-        JSONObject objet = new JSONObject();
-        try {
-            objet.put("code_user", Integer.parseInt(codeCompte));
-            objetAEnvoyer.put(objet);
-        } catch (JSONException e) {
-            // l'exception ne doit pas se produire
-            toutOk = false;
-        }
-        if (toutOk) {
-            System.out.println(objetAEnvoyer.toString());
-            /*
-             * préparation du client modifié, en tant qu'objet Json
-             * Les informations le concernant sont renseignées avec des valeurs par
-             * défaut,
-             * sauf le nom du magasin qui est celui renseigné par l'utilisateur
-             */
             JsonArrayRequest requeteVolley = new JsonArrayRequest(Request.Method.GET,
-                    URL_LAST_HUMEUR, objetAEnvoyer,
+                    url, null,
                     // Ecouteur pour la réception de la réponse de la requête
                     new com.android.volley.Response.Listener<JSONArray>() {
                         @Override
                         public void onResponse(JSONArray reponse) {
-                            // la zone de résultat est renseignée avec le résultat
-//                            aaa.setText(reponse.toString());
-//                            StringBuilder resultatFormate = new StringBuilder();
-//                            try {
-//                                resultatFormate.append(reponse.getString("infos"));
-//
-//                            } catch (JSONException e) {
-//                                throw new RuntimeException(e);
-//                            }
-//                            aaa.setText(resultatFormate.toString());
-                            System.out.println("yesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+                            System.out.println(reponse.toString());
+                            setZoneResultatAvecObjetJson(reponse);
                         }
                     },
                     // Ecouteur en cas d'erreur
@@ -219,7 +195,7 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
                 @Override
                 public Map getHeaders() throws AuthFailureError {
                     HashMap<String, String> headers = new HashMap<>();
-                    headers.put("HTTP_CYMAPIKEY", apiKey);
+                    headers.put("CYMAPIKEY", apiKey);
                     System.out.println(headers.toString());
                     return headers;
                 }
@@ -235,7 +211,7 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
             };
             // ajout de la requête dans la file d'attente Volley
             getFileRequete().add(requeteVolley);
-        }
+
     }
     public void recupererHumeurs(String codeCompte, String apikey) {
 //        zoneAleatoire.setText(getString(R.string.message_communication) + nombre);
@@ -249,5 +225,136 @@ public class FragmentHistorique extends Fragment implements AdapterView.OnItemCl
         }
         // sinon
         return fileRequete;
+    }
+
+    public void setZoneResultatAvecObjetJson(JSONArray reponse) {
+//        try {
+//            JSONObject humeurs = new JSONObject();
+//            humeurs.put("Humeur_Libelle", "ramette papier");
+//            ligneCommande1.put("quantite", 10);
+//            ligneCommande1.put("prix", 4.5);
+//            // Eventuellement pour convertir en chaîne de caractères :
+//            String json = ligneCommande.toString() ;
+//
+//
+//
+//            JSONObject objetJson = new JSONObject();
+            JSONObject ligneHumeurs1 = new JSONObject();
+        JSONObject ligneHumeurs2 = new JSONObject();
+        JSONObject ligneHumeurs3 = new JSONObject();
+        JSONObject ligneHumeurs4 = new JSONObject();
+        JSONObject ligneHumeurs5 = new JSONObject();
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+//            for (int i = 0; i < reponse.length(); i++) {
+//                try {
+//                    // on récupère l’objet Json situé en position i dans le tableau
+//                    System.out.println(reponse.getJSONObject(i));
+//
+//                } catch(JSONException erreur) {
+////                    Log.i(TAG_LOG, "Problème lors de l'analyse JSON");
+//                }
+//            }
+        try {
+            ligneHumeurs1 = reponse.getJSONObject(0);
+            ligneHumeurs2 = reponse.getJSONObject(1);
+            ligneHumeurs3 = reponse.getJSONObject(2);
+            ligneHumeurs4 = reponse.getJSONObject(3);
+            ligneHumeurs5 = reponse.getJSONObject(4);
+
+            descriptionHumeurs = new ArrayList<>();
+            descriptionHumeurs.add(ligneHumeurs1.getString("Humeur_Description"));
+            descriptionHumeurs.add(ligneHumeurs2.getString("Humeur_Description"));
+            descriptionHumeurs.add(ligneHumeurs3.getString("Humeur_Description"));
+            descriptionHumeurs.add(ligneHumeurs4.getString("Humeur_Description"));
+            descriptionHumeurs.add(ligneHumeurs5.getString("Humeur_Description"));
+            System.out.println(descriptionHumeurs.toString());
+
+            //On déclare la HashMap qui contiendra les informations pour un item
+            HashMap<String, String> map;
+
+            //Création d'une HashMap pour insérer les informations du premier item de notre listView
+            map = new HashMap<String, String>();
+            //on insère un élément emoji que l'on récupérera dans le textView emoji créé dans le fichier row_item.xml
+            map.put("emoji", ligneHumeurs1.getString("Humeur_Emoji"));
+            //on insère un élément humeur que l'on récupérera dans le textView humeur créé dans le fichier row_item.xml
+            map.put("humeur", ligneHumeurs1.getString("Humeur_Libelle"));
+            //on insère un élément date que l'on récupérera dans le textView date créé dans le fichier row_item.xml
+            map.put("date", ligneHumeurs1.getString("Humeur_Time"));
+            //on ajoute cette hashMap dans la arrayList
+            listItem.add(map);
+
+            map = new HashMap<String, String>();
+            map.put("emoji", ligneHumeurs2.getString("Humeur_Emoji"));
+            map.put("humeur", ligneHumeurs2.getString("Humeur_Libelle"));
+            map.put("date", ligneHumeurs2.getString("Humeur_Time"));
+            listItem.add(map);
+
+            map = new HashMap<String, String>();
+            map.put("emoji", ligneHumeurs3.getString("Humeur_Emoji"));
+            map.put("humeur", ligneHumeurs3.getString("Humeur_Libelle"));
+            map.put("date", ligneHumeurs3.getString("Humeur_Time"));
+            listItem.add(map);
+
+            map = new HashMap<String, String>();
+            map.put("emoji", ligneHumeurs4.getString("Humeur_Emoji"));
+            map.put("humeur", ligneHumeurs4.getString("Humeur_Libelle"));
+            map.put("date", ligneHumeurs4.getString("Humeur_Time"));
+            listItem.add(map);
+
+            map = new HashMap<String, String>();
+            map.put("emoji", ligneHumeurs5.getString("Humeur_Emoji"));
+            map.put("humeur", ligneHumeurs5.getString("Humeur_Libelle"));
+            map.put("date", ligneHumeurs5.getString("Humeur_Time"));
+            listItem.add(map);
+
+            //Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre list (listItem) dans la vue row_item
+            SimpleAdapter adaptateur = new SimpleAdapter (getActivity(), listItem, R.layout.row_item,
+                    new String[] {"emoji", "humeur", "date"}, new int[] {R.id.emoji, R.id.humeur, R.id.date});
+
+            //On attribue à notre listView l'adapter que l'on vient de créer
+            listeHumeurs.setAdapter(adaptateur);
+
+        } catch(JSONException erreur) {
+        }
+
+
+//        try {
+//            StringBuilder resultatFormate = new StringBuilder();
+//            /*
+//             * on extrait de l'objet Json reponse : le titre, l'année, les auteurs
+//             * On construit la chaine resultatFormate avec des libellés et le chaînes
+//             * extraites de l'objet Json
+//             */
+//            resultatFormate.append(reponse.getString("APIKEY"));
+////            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa : " + resultatFormate);
+//            // on affiche la chaîne fomratée
+//            test.setText(resultatFormate.toString());
+//            apiKey = resultatFormate.toString();
+////            System.out.println("codeCompteGetApiKey : " + codeCompte);
+//            System.out.println("apikeyGetApiKey : " + apiKey);
+////            getFiveHumeurs();
+//        } catch(JSONException erreur) {
+//            test.setText("joqhboqupehbv");
+//        }
+
+
+//            StringBuilder resultatFormate = new StringBuilder();
+//            /*
+//             * on extrait de l'objet Json reponse : le titre, l'année, les auteurs
+//             * On construit la chaine resultatFormate avec des libellés et le chaînes
+//             * extraites de l'objet Json
+//             */
+//            resultatFormate.append(reponse.getString("APIKEY"));
+////            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa : " + resultatFormate);
+//            // on affiche la chaîne fomratée
+//            test.setText(resultatFormate.toString());
+//            apiKey = resultatFormate.toString();
+////            System.out.println("codeCompteGetApiKey : " + codeCompte);
+//            System.out.println("apikeyGetApiKey : " + apiKey);
+////            getFiveHumeurs();
+//        } catch(JSONException erreur) {
+////            test.setText("joqhboqupehbv");
+//        }
     }
 }
